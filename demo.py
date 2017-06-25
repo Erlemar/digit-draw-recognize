56fa043b0a34100a9ebd505e31a3006b2429c46f
Erlemar <and-lukyane@yandex.ru>
2017-06-25 19:39:03 +0300

    5
    5

--- demo.py
+++ demo.py
@@ -34,9 +34,9 @@ def crossdomain(origin=None, methods=None, headers=None,
                 automatic_options=True):
     if methods is not None:
         methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, basestring):
+    if headers is not None and not isinstance(headers, str):
         headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, basestring):
+    if not isinstance(origin, str):
         origin = ', '.join(origin)
     if isinstance(max_age, timedelta):
         max_age = max_age.total_seconds()
