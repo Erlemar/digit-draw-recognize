828b870eed3603177288efee8ea64c3e2b872ac5
Erlemar <and-lukyane@yandex.ru>
2017-06-25 19:27:36 +0300

    cors
    cors

--- demo.py
+++ demo.py
@@ -18,13 +18,16 @@ import boto
 import boto3
 from boto.s3.key import Key
 from boto.s3.connection import S3Connection
+from flask.ext.cors import CORS, cross_origin
 app = Flask(__name__)
-
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 #start_time = time.time()
 #classifier = SentimentClassifier()
 print("Functionality is ready, loading took {0} seconds.".format(time.time() - start_time))
 
 @app.route("/", methods=["POST", "GET"])
+@cross_origin()
 def index_page(text="", prediction_message=""):
 	if request.method == "POST":
 		text = request.form["text"]
@@ -47,11 +50,37 @@ def index_page(text="", prediction_message=""):
 	return render_template('hello.html', text=text, prediction_message=prediction_message)
 
 @app.route('/hook', methods = ["GET", "POST"])
+@cross_origin()
 def get_image():
 	if request.method == 'POST':
 		image_b64 = request.values['imageBase64']
+		image_encoded = image_b64.split(',')[1]
+		image = base64.decodebytes(image_encoded.encode('utf-8'))
+		'digit1-O_n1'.split('n')
+		drawn_digit = request.values['digit']
+		type = 'O'
+		filename = 'digit' + str(drawn_digit) + '-' + type + str(uuid.uuid1()) + '.jpg'
+		with open('tmp/' + filename, 'wb') as f:
+			f.write(image)
 
-	return os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY']
+		REGION_HOST = 's3-external-1.amazonaws.com'
+		#S3_BUCKET = os.environ.get('S3_BUCKET')
+		#AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+		#AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+		#return AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+		#conn = S3Connection(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, host=REGION_HOST)
+		conn = S3Connection('AKIAIRXR5PJRWK6YVZMA', 'hKjhSC6Rtqvkww4JcgQOfVfQLocGm8GBUEEWgtmc', host=REGION_HOST)
+		#conn = S3Connection(os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'], host=REGION_HOST)
+		#conn = S3Connection(os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'], host=REGION_HOST)
+		bucket = conn.get_bucket('digit_draw_recognize')
+		#print(bucket, os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'])
+		k = Key(bucket)
+		key = filename
+		fn = 'tmp/' + filename
+		k.key = key
+		k.set_contents_from_filename(fn)
+		print('Done')
+	return filename
 
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
