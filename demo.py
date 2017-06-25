4d92dd6fa9eac2e965404cc9a307a95d65a52f26
Erlemar <and-lukyane@yandex.ru>
2017-06-25 20:19:15 +0300

    again
    again

--- demo.py
+++ demo.py
@@ -29,11 +29,11 @@ print("Functionality is ready, loading took {0} seconds.".format(time.time() - s
 #CORS_HEADERS = 'Content-Type'
 
 #cors = CORS(app, resources={r"/foo": {"origins": "http://localhost:port"}})
-
+CORS(app, headers=['Content-Type'])
 @app.route("/", methods=["POST", "GET"])
 #@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def index_page(text="", prediction_message=""):
-	response.headers.add('Access-Control-Allow-Origin', '*')
+	#request.headers.add('Access-Control-Allow-Origin', '*')
 	if request.method == "POST":
 		text = request.form["text"]
 		logfile = open("demo_logs.txt", "a", "utf-8")
@@ -57,7 +57,7 @@ def index_page(text="", prediction_message=""):
 @app.route('/hook', methods = ["GET", "POST"])
 #@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def get_image():
-	response.headers.add('Access-Control-Allow-Origin', '*')
+	#request.headers.add('Access-Control-Allow-Origin', '*')
 	if request.method == 'POST':
 		image_b64 = request.values['imageBase64']
 		image_encoded = image_b64.split(',')[1]
