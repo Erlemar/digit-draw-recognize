7975e9bf09cfa30d9b77a1aed39e4e182c003a7c
Erlemar <and-lukyane@yandex.ru>
2017-06-25 18:04:59 +0300

    another attempt
    another attempt

--- demo.py
+++ demo.py
@@ -61,10 +61,20 @@ def get_image():
 
 		REGION_HOST = 's3-external-1.amazonaws.com'
 		#S3_BUCKET = os.environ.get('S3_BUCKET')
-		AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-		AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-	return AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
-
+		#AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+		#AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+		#conn = S3Connection(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, host=REGION_HOST)
+		#conn = S3Connection(os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'], host=REGION_HOST)
+		conn = S3Connection(os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'], host=REGION_HOST)
+		bucket = conn.get_bucket('digit_draw_recognize')
+		#print(bucket, os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'])
+		k = Key(bucket)
+		key = filename
+		fn = 'tmp/' + filename
+		k.key = key
+		k.set_contents_from_filename(fn)
+		print('Done')
+	return filename
 
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
