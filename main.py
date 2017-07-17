f0da08f743387eb8ff225ee55ccd24cac6c33ef8
Erlemar <and-lukyane@yandex.ru>
2017-07-17 21:41:11 +0300

    .

--- main.py
+++ main.py
@@ -17,27 +17,43 @@ CORS(app, headers=['Content-Type'])
 
 @app.route("/", methods=["POST", "GET", 'OPTIONS'])
 def index_page():
+
 	return render_template('index.html')
 	
 @app.route("/about")
 def about():
+
 	return render_template('about.html')
 	
 @app.route("/internals")
 def internals():
+
 	return render_template('internals.html')
 
 @app.route("/models")
 def models():
+
 	return render_template('models.html')
 
-@app.route('/hook2', methods = ["GET", "POST", 'OPTIONS'])
-def predict():
-	"""
-	Decodes image and uses it to make prediction.
-	"""
+'''
+@app.route('/hook', methods = ["GET", "POST", 'OPTIONS'])
+#@cross_origin(origin='https://digits-draw-recognize.herokuapp.com')
+def get_image():
 	if request.method == 'POST':
 		image_b64 = request.values['imageBase64']
+		drawn_digit = request.values['digit']
+		#print('Data received')
+		image_encoded = image_b64.split(',')[1]
+		image = base64.decodebytes(image_encoded.encode('utf-8'))		
+		save = model.save_image(drawn_digit, image)	
+
+	return save
+'''
+@app.route('/hook2', methods = ["GET", "POST", 'OPTIONS'])
+def predict():
+	if request.method == 'POST':
+		image_b64 = request.values['imageBase64']
+		print('Sending data to script')
 		image_encoded = image_b64.split(',')[1]
 		image = base64.decodebytes(image_encoded.encode('utf-8'))		
 		prediction = model.predict(image)	
@@ -46,9 +62,6 @@ def predict():
 	
 @app.route('/hook3', methods = ["GET", "POST", 'OPTIONS'])
 def train():
-	"""
-	Decodes image and uses it to tain models.
-	"""
 	if request.method == 'POST':
 		image_b64 = request.values['imageBase64']
 		image_encoded = image_b64.split(',')[1]
