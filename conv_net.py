4fa59b21194036ca5d3c15f1b993bb35ad732a15
Erlemar <and-lukyane@yandex.ru>
2017-07-18 20:57:23 +0300

    Various description fixes
    Various description fixes

--- conv_net.py
+++ conv_net.py
@@ -8,77 +8,37 @@ import os
 
 class CNN(object):
 	"""
-	A two-layer fully-connected neural network. The net has an input dimension of
-	N, a hidden layer dimension of H, and performs classification over C classes.
-	We train the network with a softmax loss function and L2 regularization on the
-	weight matrices. The network uses a ReLU nonlinearity after the first fully
-	connected layer.
-	In other words, the network has the following architecture:
-	input - fully connected layer - ReLU - fully connected layer - softmax
+	Convolutional neural network with several levels. The architectureis the following:
+	
+	input - conv - relu - pool - dropout - conv - relu - pool - dropout - fully connected layer - dropout - fully connected layer
+
 	The outputs of the second fully-connected layer are the scores for each class.
 	"""
 
 	def __init__(self):
 		"""
-		Initialize the model. Weights are initialized to small random values and
-		biases are initialized to zero. Weights and biases are stored in the
-		variable self.params, which is a dictionary with the following keys:
-		W1: First layer weights; has shape (D, H)
-		b1: First layer biases; has shape (H,)
-		W2: Second layer weights; has shape (H, C)
-		b2: Second layer biases; has shape (C,)
-		Inputs:
-		- input_size: The dimension D of the input data.
-		- hidden_size: The number of neurons H in the hidden layer.
-		- output_size: The number of classes C.
+		Initialize the model. Weights are passed into the class.
 		"""
 		self.params = {}
 
-	def model(self, X, w, w3, w4, w_o, p_keep_conv, p_keep_hidden):
-		l1a = tf.nn.relu(tf.nn.conv2d(X, w, strides=[1, 1, 1, 1], padding='SAME') + b1)
-		l1 = tf.nn.max_pool(l1a, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-		l1 = tf.nn.dropout(l1, p_keep_conv)
-
-		l3a = tf.nn.relu(tf.nn.conv2d(l1, w3, strides=[1, 1, 1, 1], padding='SAME') + b3)
-		l3 = tf.nn.max_pool(l3a, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-		l3 = tf.reshape(l3, [-1, w4.get_shape().as_list()[0]])    # reshape to (?, 2048)
-		l3 = tf.nn.dropout(l3, p_keep_conv)
-
-		l4 = tf.nn.relu(tf.matmul(l3, w4) + b4)
-		l4 = tf.nn.dropout(l4, p_keep_hidden)
-
-		pyx = tf.matmul(l4, w_o) + b5
-		return pyx
-
 	def train(self, image, digit):
 		"""
-		Train this neural network using stochastic gradient descent.
+		Train this neural network. 1 step of gradient descent.
+
 		Inputs:
-		- X: A numpy array of shape (N, D) giving training data.
+		- X: A numpy array of shape (N, 784) giving training data.
 		- y: A numpy array f shape (N,) giving training labels; y[i] = c means that
 		  X[i] has label c, where 0 <= c < C.
-		- X_val: A numpy array of shape (N_val, D) giving validation data.
-		- y_val: A numpy array of shape (N_val,) giving validation labels.
-		- learning_rate: Scalar giving learning rate for optimization.
-		- reg: Scalar giving regularization strength.
-		- num_iters: Number of steps to take when optimizing.
-		- batch_size: Number of training examples to use per step.
-		- verbose: boolean; if true print progress during optimization.
 		"""
 
-		#f = 'data-all_2_updated.chkp'
 		tf.reset_default_graph()
 		init_op = tf.global_variables_initializer()
-		#sess = tf.Session()
 
 		X = tf.placeholder("float", [None, 28, 28, 1])
 		Y = tf.placeholder("float", [None, 10])
 
 		w = tf.get_variable("w", shape=[4, 4, 1, 16], initializer=tf.contrib.layers.xavier_initializer())
 		b1 = tf.get_variable(name="b1", shape=[16], initializer=tf.zeros_initializer())
-		#w2 = tf.get_variable("w2", shape=[5, 5, 32, 64],
-		#           initializer=tf.contrib.layers.xavier_initializer())
-		#b2 = tf.get_variable(name="b2", shape=[64], initializer=tf.zeros_initializer())
 		w3 = tf.get_variable("w3", shape=[4, 4, 16, 32], initializer=tf.contrib.layers.xavier_initializer())
 		b3 = tf.get_variable(name="b3", shape=[32], initializer=tf.zeros_initializer())
 		w4 = tf.get_variable("w4", shape=[32 * 7 * 7, 625], initializer=tf.contrib.layers.xavier_initializer())
@@ -111,10 +71,9 @@ class CNN(object):
 		predict_op = tf.argmax(py_x, 1)
 		
 		with tf.Session() as sess:
-			# you need to initialize all variables
 			saver = tf.train.Saver()
 			saver.restore(sess, "./tmp/data-all_2_updated.chkp")
-			trX = image.reshape(-1, 28, 28, 1)  # 28x28x1 input img
+			trX = image.reshape(-1, 28, 28, 1)
 			trY = np.eye(10)[digit]
 			sess.run(train_op, feed_dict={X: trX, Y: trY,
 											  p_keep_conv: 1, p_keep_hidden: 1})
@@ -122,6 +81,11 @@ class CNN(object):
 			all_saver.save(sess, './tmp/data-all_2_updated.chkp')
 
 	def predict(self, image, weights='original'):
+		"""
+		Generate prediction for one or several digits. Weights are downloaded from Amazon.
+		Returns:
+		- top_3: a list of 3 top most probable predictions with their probabilities as tuples.
+		"""
 		s3 = boto3.client('s3', aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
 		if weights == 'original':
 			f = 'data-all_2.chkp'
@@ -142,9 +106,6 @@ class CNN(object):
 
 		w = tf.get_variable("w", shape=[4, 4, 1, 16], initializer=tf.contrib.layers.xavier_initializer())
 		b1 = tf.get_variable(name="b1", shape=[16], initializer=tf.zeros_initializer())
-		#w2 = tf.get_variable("w2", shape=[5, 5, 32, 64],
-		#           initializer=tf.contrib.layers.xavier_initializer())
-		#b2 = tf.get_variable(name="b2", shape=[64], initializer=tf.zeros_initializer())
 		w3 = tf.get_variable("w3", shape=[4, 4, 16, 32], initializer=tf.contrib.layers.xavier_initializer())
 		b3 = tf.get_variable(name="b3", shape=[32], initializer=tf.zeros_initializer())
 		w4 = tf.get_variable("w4", shape=[32 * 7 * 7, 625], initializer=tf.contrib.layers.xavier_initializer())
@@ -167,7 +128,6 @@ class CNN(object):
 		l4 = tf.nn.dropout(l4, p_keep_hidden)
 
 		py_x = tf.matmul(l4, w_o) + b5
-		#py_x = self.model(X, w, w3, w4, w_o, p_keep_conv, p_keep_hidden)
 
 		reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
 		reg_constant = 0.01
